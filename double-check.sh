b1ce242c7716df7d7cb333be1048249ea1102d5e
Jim <blyzzcanada@gmail.com>
2024-02-25 00:09:47 -0800

    Update double-check.sh

--- double-check.sh
+++ double-check.sh
@@ -2,10 +2,10 @@
 
 # Check if Apache script is not running
 if ! pgrep -f "apache.sh" > /dev/null ; then
-    screen -S Apache -d -m bash -c "/home/kali/apache.sh"
+    /usr/bin/screen -S "Apache" -d -m bash -c "/home/kali/apache.sh"
 fi
 
 # Check if IIS script is not running
 if ! pgrep -f "iis.sh" > /dev/null ; then
-    screen -S IIS -d -m bash -c "/home/kali/iis.sh"
+    /usr/bin/screen -S "IIS" -d -m bash -c "/home/kali/iis.sh"
 fi
